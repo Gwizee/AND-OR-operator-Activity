90af8e7c6f0d663b79df7158b258616e758f6142
Gwizee <risingfire69@gmail.com>
2024-10-10 21:19:11 +0600

    AND-OR Operator Activity has been completed

---
+++ main.py
@@ -0,0 +1,22 @@
+a = 2
+b = 10
+c = 0
+
+if a and b and c :
+    print("All the data are True")
+else :
+    print("All the data are False")
+
+x = 2
+y = 10
+z = 0
+
+if x > 0 or y > 0 :
+    print("x or y is greater than 0")
+else :
+    print("x and y is not greater than 0")
+
+if x > y or y > z :
+    print("x or y is greater than z")
+else :
+    print("x or y is not greater than z")
